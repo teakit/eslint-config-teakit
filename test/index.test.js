3b6d32f5b53c2b7e52f40ddad8d6e87320516427
Billgo <cocobill@vip.qq.com>
2020-10-01 19:11:39 +0800

    fixed: test

--- test/index.test.js
+++ test/index.test.js
@@ -7,17 +7,6 @@ describe("test/index.test.js", () => {
   const cwd = path.join(__dirname, "fixtures/normal-app");
 
   describe("comma dangle", () => {
-    it("should fail when the parameter of fn has trailing comma", () => {
-      return (
-        coffee
-          .spawn("eslint", ["comma-dangle-fn-fail.js"], { cwd })
-          // .debug()
-          .expect("stdout", /Unexpected trailing comma {2}comma-dangle/)
-          .expect("code", 1)
-          .end()
-      );
-    });
-
     it("should success", () => {
       return (
         coffee
